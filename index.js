b66781956f7a21c90c7e404014cd12b733e78106
RomIvaNeirity <neirity@gmail.com>
2025-09-25 18:54:23 +0000

    Deploying to gh-pages from @ RomIvaNeirity/goit-js-hw-12@01b3a9b71cd6d91eab74658fd4774dc463337138 🚀

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-import{a as m,S as f,i}from"./assets/vendor-jlerdHUr.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))u(t);new MutationObserver(t=>{for(const s of t)if(s.type==="childList")for(const l of s.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&u(l)}).observe(document,{childList:!0,subtree:!0});function a(t){const s={};return t.integrity&&(s.integrity=t.integrity),t.referrerPolicy&&(s.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?s.credentials="include":t.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function u(t){if(t.ep)return;t.ep=!0;const s=a(t);fetch(t.href,s)}})();async function y(r){return(await m({method:"get",url:"https://pixabay.com/api/",params:{key:"28573378-d6ea96aa58230c7b9cb77205d",q:r,image_type:"photo",orientation:"horizontal",safesearch:!0}})).data}const p=document.querySelector(".gallery");function g(r){let e="";r.map(a=>{e=e+`<li class="gallery-item">
-  <a class="gallery-link" href="${a.largeImageURL}">
+import{a as L,S as w,i}from"./assets/vendor-jlerdHUr.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const o of document.querySelectorAll('link[rel="modulepreload"]'))c(o);new MutationObserver(o=>{for(const r of o)if(r.type==="childList")for(const m of r.addedNodes)m.tagName==="LINK"&&m.rel==="modulepreload"&&c(m)}).observe(document,{childList:!0,subtree:!0});function s(o){const r={};return o.integrity&&(r.integrity=o.integrity),o.referrerPolicy&&(r.referrerPolicy=o.referrerPolicy),o.crossOrigin==="use-credentials"?r.credentials="include":o.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function c(o){if(o.ep)return;o.ep=!0;const r=s(o);fetch(o.href,r)}})();async function p(t,e=1){return(await L({method:"get",url:"https://pixabay.com/api/",params:{key:"28573378-d6ea96aa58230c7b9cb77205d",q:t,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:"15",page:e}})).data}const y=document.querySelector(".gallery");function h(t){let e="";t.map(s=>{e=e+`<li class="gallery-item">
+  <a class="gallery-link" href="${s.largeImageURL}">
     <img
       class="gallery-image"
-      src="${a.webformatURL}"
-      alt="${a.tags}"
-      data-user="Uploaded by user: ${a.user}"
+      src="${s.webformatURL}"
+      alt="${s.tags}"
+      data-user="Uploaded by user: ${s.user}"
     />
   </a>
   <ul class="image-attribute-list">
   <li class="image-attribute-list-item">
   <p>Likes</p>
-  <p>${a.likes}</p>
+  <p>${s.likes}</p>
   </li>
   <li class="image-attribute-list-item">
   <p>Views</p>
-  <p>${a.views}</p>
+  <p>${s.views}</p>
   </li>
   <li class="image-attribute-list-item">
   <p>Comments</p>
-  <p>${a.comments}</p>
+  <p>${s.comments}</p>
   </li>
   <li class="image-attribute-list-item">
   <p>Downloads</p>
-  <p>${a.downloads}</p>
+  <p>${s.downloads}</p>
   </li>
   </ul> 
-</li>`}).join(""),p.insertAdjacentHTML("beforeend",e),h.refresh()}const h=new f(".gallery-link",{captions:!0,captionsData:"data-user",captionDelay:250});function b(){p.innerHTML=""}const d=document.querySelector("span");function L(){d.style.display="block"}function o(){d.style.display="none"}o();let n;const c=document.querySelector("form");c.addEventListener("submit",w);async function w(r){if(r.preventDefault(),b(),n=r.target.elements["search-text"].value.trim(),n.trim()===""){i.show({message:"Please, type some text"}),c.reset();return}L();try{const e=await y(n);if(!e.hits.length){o(),i.show({message:"Sorry, there are no images matching your search query. Please try again!"});return}g(e.hits),o()}catch(e){S(e)}finally{o(),c.reset()}}function S(r){let e="";r.response?e=`Server error: ${r.response.status}`:r.request?e="No response from server. Please try again later.":e=`Request error: ${r.message}`,console.error("Axios error:",r),i.show({message:e,backgroundColor:"red"})}i.settings({position:"center",backgroundColor:"magenta",messageSize:"20",timeout:3e3});
+</li>`}).join(""),y.insertAdjacentHTML("beforeend",e),v.refresh()}const v=new w(".gallery-link",{captions:!0,captionsData:"data-user",captionDelay:250});function S(){y.innerHTML=""}const f=document.querySelector(".loader");function g(){f.style.display="block"}function l(){f.style.display="none"}l();let d,n=1;const u=document.querySelector("form");u.addEventListener("submit",q);const a=document.querySelector(".load-more-button");a.addEventListener("click",$);async function q(t){if(t.preventDefault(),S(),a.classList.add("load-more-button-hidden"),i.destroy(),d=t.target.elements["search-text"].value.trim(),d.trim()===""){i.show({message:"Please, type some text"}),u.reset();return}g();try{const e=await p(d,n);if(!e.hits.length){l(),i.show({message:"Sorry, there are no images matching your search query. Please try again!"}),a.classList.add("load-more-button-hidden");return}h(e.hits),l();const s=Math.ceil(e.totalHits/15);n>=s?(a.classList.add("load-more-button-hidden"),i.show({timeout:0,position:"bottomCenter",message:"We're sorry, but you've reached the end of search results."})):a.classList.remove("load-more-button-hidden")}catch(e){b(e)}finally{l(),u.reset()}}async function $(){n+=1,g();try{const t=await p(d,n);h(t.hits),a.classList.add("load-more-button-hidden"),l();const s=document.querySelector("li").getBoundingClientRect();window.scrollBy({top:s.height*2,behavior:"smooth"});const c=Math.ceil(t.totalHits/15);n>=c?(a.classList.add("load-more-button-hidden"),i.show({timeout:0,position:"bottomCenter",message:"We're sorry, but you've reached the end of search results."})):a.classList.remove("load-more-button-hidden")}catch(t){b(t)}finally{l(),u.reset()}}function b(t){let e="";t.response?e=`Server error: ${t.response.status}`:t.request?e="No response from server. Please try again later.":e=`Request error: ${t.message}`,console.error("Axios error:",t),i.show({message:e,backgroundColor:"red"})}i.settings({position:"center",backgroundColor:"magenta",messageSize:"20",timeout:3e3});
 //# sourceMappingURL=index.js.map
